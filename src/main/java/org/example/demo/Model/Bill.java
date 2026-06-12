2d5e238aafd358cb25d65de74d890ab504812071
Ju4Cr0 <ju4cr0@gmail.com>
2026-06-12 01:08:09 -0500

    getters y setters de Bill

--- src/main/java/org/example/demo/Model/Bill.java
+++ src/main/java/org/example/demo/Model/Bill.java
@@ -26,8 +26,71 @@ public class Bill {
         this.total = 0;
         this.iva = iva;
 
-
-
     }
 
+    public double calcularSubTotal() { return 0;}
+
+    public String getIdFactura() {
+        return idFactura;
+    }
+
+    public void setIdFactura(String idFactura) {
+        this.idFactura = idFactura;
+    }
+
+    public Date getFechaEmision() {
+        return fechaEmision;
+    }
+
+    public void setFechaEmision(Date fechaEmision) {
+        this.fechaEmision = fechaEmision;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public float getIva() {
+        return iva;
+    }
+
+    public void setIva(float iva) {
+        this.iva = iva;
+    }
+
+    public Status getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Status estado) {
+        this.estado = estado;
+    }
+
+    public String getNumeroFactura() {
+        return numeroFactura;
+    }
+
+    public void setNumeroFactura(String numeroFactura) {
+        this.numeroFactura = numeroFactura;
+    }
+
+    public float getSubTotal() {
+        return subTotal;
+    }
+
+    public void setSubTotal(float subTotal) {
+        this.subTotal = subTotal;
+    }
+
+    public float getTotal() {
+        return total;
+    }
+
+    public void setTotal(float total) {
+        this.total = total;
+    }
 }
